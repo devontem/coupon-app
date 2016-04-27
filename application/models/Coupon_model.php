a3ab24f3f4a54b855796972ea367ba238d10d647
Devonte M <devontem@live.com>
2016-04-27 11:55:44 -0400

    added model logic to coupon model

--- application/models/Coupon_model.php
+++ application/models/Coupon_model.php
@@ -1,9 +1,38 @@
 <?php // omit closing php tag
 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
 class Coupon_model extends CI_Model(){
 	
 	// constructing parent class
 	function __construct(){
 		parent::__construct();
+	}
+
+	public function get_all(){
+		$query = $this->db->get('coupons');
+		return $query->result();
+
+	}
+
+	public function get_by_id($id){
+		$this->db->where('id', $id);
+		$query = $this->db->get('coupons');
+
+		return $query->result();
+	}
+
+	public function update($id, $data){
+		$this->db->where('id', $id);
+		$this->db->update('coupons', $data);
+	}
+
+	public function delete($id){
+		// Permanently deleted record
+		// $this->db->delete('coupons', array('id' => $id));
+
+		// Flags record as inactive
+		$this->db->where('id', $id);
+		$this->db->update('coupons', array('status' => 0));
 	}
 }
