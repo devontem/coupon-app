3062f5d668e40ee4d743e0e8b8f0a7ad46cf271c
Devonte M <devontem@live.com>
2016-04-28 10:56:21 -0400

    added main GET factories

--- application/views/index.php
+++ application/views/index.php
@@ -22,7 +22,7 @@
 			    <div class="nav-wrapper">
 			      <a href="#" class="brand-logo center">Coupon-Galore!</a>
 			      <ul id="nav-mobile" class="right hide-on-med-and-down">
-			        <li><a href="sass.html">Create a new coupon!</a></li>
+			        <li><a href="/#/new">Create a new coupon!</a></li>
 			      </ul>
 			    </div>
 			  </nav>
--- assets/js/app.js
+++ assets/js/app.js
@@ -20,11 +20,31 @@ angular.module('app', ['ui.router'])
 
 .controller('MainController', function($scope, Coupon){
 
-	$scope.coupons = [1,2,3,4,5];
+	// $scope.coupons = [1,2,3,4,5];
+	Coupon.getApiCoupons().then(function(data){
+		$scope.coupons = data;
+		console.log(data)
+	})
 	$scope.form = {};
 
 })
 
-.factory('Coupon', function($scope){
+.factory('Coupon', function($http){
 
+	var getUserCoupons = function(){
+		return $http.get('/api/coupons').then(function(data){
+			return data.data;
+		});
+	}
+
+	var getApiCoupons = function(){
+		return $http.get('/api/couponapi').then(function(data){
+			return data.data
+		})
+	}
+
+	return {
+		getUserCoupons: getUserCoupons,
+		getApiCoupons: getApiCoupons
+	}
 });
